9a53f0abff1b798d1607eb745c9c7bf0c88963b1
Nes1998 <fong.nestor@yahoo.com>
2022-09-05 19:45:12 -0500

    Adding colors to the project

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -16,7 +16,16 @@ module.exports = {
         "fade-in": {
           "0%": {opacity: 0},
           "100%": {opacity: 1}
-        }
+        },
+      },
+      color: {
+        "sky-50": "#ecfeff",
+        "sky-100": "#cffafe",
+        "sky-200": "#bae6fd",
+        "sky-300": "#7dd3fc",
+        "sky-400": "#38bdf8",
+        "sky-500": "#0ea5e9" ,
+        "cyan-50": "#ecfeff"
       }
     },
   },
