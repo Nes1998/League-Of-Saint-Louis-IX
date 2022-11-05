960f6b1e08eab1abd18e4d7fecebb960577d0c65
Nes1998 <fong.nestor@yahoo.com>
2022-11-05 18:05:19 -0500

    Starting work on the backend for the league page

---
+++ backend/models/SignUpModels.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const singUpTemplate = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
