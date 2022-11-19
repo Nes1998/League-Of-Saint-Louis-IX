6c81ae1eacab9881baeeabe27ca3f7611e4d7d38
Nes1998 <fong.nestor@yahoo.com>
2022-11-19 17:48:15 -0600

    Working on the backend

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,7 +4,17 @@ const singUpTemplateCopy = require('../models/SignUpModels')
 
 router.post('/signup', (request, response) => {
     const signedUpUser = new singUpTemplateCopy({
-        name: request.body.name
+        name: request.body.name,
+        email: request.body.email,
+        phoneNumber: request.body.phoneNumber,
+        address: request.body.address,
+        occupation: request.body.occupation
+    })
+
+    signedUpUser.save().then(data => {
+        response.json(data)
+    }).catch(error => {
+        response.json(error)
     })
 })
 
